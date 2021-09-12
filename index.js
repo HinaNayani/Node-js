7e6aa687609963d024ecace162e95d02796b40b8
HinaNayani <90512860+HinaNayani@users.noreply.github.com>
2021-09-12 02:47:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//  console.log("hello world");
+
+//  const secret = "babyhippo";
+//  let guess = prompt("What is the secret code?");
+
+//  while(secret != guess){
+//     guess = prompt("what is the secret code");
+// }
+// console.log("congrats u cracked the code");
+
+for(i = 0; i <= 10; i++){
+    console.log(i);
+    for(j = 0; j <=4; j++){
+        console.log(j);
+    }
+}
+
+
