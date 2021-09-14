864c29b96e77182176c3e4d76495dddb843cd3da
HinaNayani <90512860+HinaNayani@users.noreply.github.com>
2021-09-14 05:50:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//  console.log("hello world");
+//while loop
+const secret = "babyhippo";
+let guess = prompt("What is the secret code?");
 
-//  const secret = "babyhippo";
-//  let guess = prompt("What is the secret code?");
+ while(secret != guess){
+    guess = prompt("what is the secret code");
+}
+console.log("congrats u cracked the code");
 
-//  while(secret != guess){
-//     guess = prompt("what is the secret code");
-// }
-// console.log("congrats u cracked the code");
 
+//nested loop
 for(i = 0; i <= 10; i++){
     console.log(i);
     for(j = 0; j <=4; j++){
@@ -15,4 +16,22 @@ for(i = 0; i <= 10; i++){
     }
 }
 
+//prime numbers sum
+function isPrime(num) { 
+    for(let i = 2; i < num; i++) 
+      if(num % i === 0) return false;
+     return num > 1;
+  } 
+  
+  function totalPrimeNumbers(array) { 
+    return array.reduce((acumulative, numerical) => { 
+      if (isPrime(numerical)) { 
+        acumulative += numerical;
+      }
+      return acumulative
+    }, 0); 
+  } 
+  console.log(totalPrimeNumbers([1,5,2,9,3,4,11]));
+
+ 
 
